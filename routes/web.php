d05226db2a63a9229ef6e9f04e736cf763be49cb
Gergő Tisza <gtisza@wikimedia.org>
2018-11-30 00:24:53 +0000

    Basic layout

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-$router->get('/', function () use ($router) {
+$router->get('/version', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('/progress/{wiki}', 'ApiController@wikiProgress');
+});
+
+$router->get('/', 'DashboardController@show');
